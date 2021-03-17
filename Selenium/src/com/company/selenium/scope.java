206a3cc0e957db343f03d9f845aace0a9e170d2d
DELL <DELL@Sivaram>
2021-03-17 08:33:31 +0530

    scrpit to open links in page and to get tile of open links

--- Selenium/src/com/company/selenium/scope.java
+++ Selenium/src/com/company/selenium/scope.java
@@ -1,5 +1,8 @@
 package com.company.selenium;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -8,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class scope {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\softwares\\setup of selenium\\SE_Chrome\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -19,12 +22,22 @@ public class scope {
 		System.out.println(footer.findElements(By.tagName("a")).size()); 
 		WebElement columndriver = footer.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
 		System.out.println(columndriver.findElements(By.tagName("a")).size());   
-		
-		for(int i=0;i<columndriver.findElements(By.tagName("a")).size();i++)
+		//click on each link in the column
+		for(int i=1;i<columndriver.findElements(By.tagName("a")).size();i++)
 {
-			String clickonlink=Keys.chord(Keys.CONTROL,Keys.ENTER);
-			columndriver.findElements(By.tagName("a")).get(i).click();
+			String clickonlink=Keys.chord(Keys.CONTROL,Keys.ENTER); //open all tabs
+			columndriver.findElements(By.tagName("a")).get(i).sendKeys(clickonlink);
+			Thread.sleep(5000);
+}
+			Set<String> abc=driver.getWindowHandles();
+			Iterator<String> it=abc.iterator();
+			while(it.hasNext())
+			{
+			driver.switchTo().window(it.next());
+			System.out.println(driver.getTitle());
+			}
+			
 }
 	}
 
-}
+
