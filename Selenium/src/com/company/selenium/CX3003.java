cfecb35af75cfb8fea44277dfef72bd82230c191
DELL <DELL@Sivaram>
2021-04-01 15:43:08 +0530

    Implemented script for CX30x3

--- Selenium/src/com/company/selenium/CX3003.java
+++ Selenium/src/com/company/selenium/CX3003.java
@@ -11,9 +11,15 @@ public class CX3003 {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\softwares\\setup of selenium\\SE_Chrome\\chromedriver.exe");
+		
+		int i=0, n=500;
+		for (i=1;i<n;i++)
+		{
+		Thread.sleep(10000);
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://10.100.97.64/home.php");
 		driver.manage().window().maximize();
+		Thread.sleep(10000);	
 		driver.findElement(By.id("userid")).sendKeys("admin");
 		driver.findElement(By.xpath("//input[@placeholder='Password *']")).sendKeys("admin");
 		driver.findElement(By.id("btnLogin")).click();
@@ -28,9 +34,10 @@ public class CX3003 {
 	//	a.moveToElement(SelectAll).contextClick().sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform(); //selectAll
 		a.moveToElement(SelectAll).contextClick().sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform(); //DeselctAll
 		driver.findElement(By.id("btnApplyAll")).click(); //to apply)
-		
-	//	driver.findElement(By.xpath("//span[@class='short-user']")).click();
-		//driver.findElement(By.xpath("//a[normalize-space()='Logout']")).click();  //logout
+		Thread.sleep(10000);
+     	driver.findElement(By.xpath("//span[@class='short-user']")).click();
+		driver.findElement(By.xpath("//a[normalize-space()='Logout']")).click();  //logout
+		driver.close();
 	}
 
-}
+}}
