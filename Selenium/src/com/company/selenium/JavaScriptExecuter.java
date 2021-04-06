722d84033ae359a4e3673f4ee5a5b7d6ce4795ef
DELL <DELL@Sivaram>
2021-04-06 17:20:42 +0530

    Just added when locator has double quotes

--- Selenium/src/com/company/selenium/JavaScriptExecuter.java
+++ Selenium/src/com/company/selenium/JavaScriptExecuter.java
@@ -17,20 +17,20 @@ public class JavaScriptExecuter {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		JavascriptExecutor js = (JavascriptExecutor)driver;
-		js.executeScript("window.scrollBy(0,500)");
+		js.executeScript("window.scrollBy(0,500)");    //to scroll
 		Thread.sleep(3000);
-		js.executeScript("document.querySelector(\".tableFixHead\").scrollTop=5000");
-		
+		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000"); 
+		//js.executeScript("document.querySelector(\".tableFixHead\").scrollTop=5000"); //to identify table and scroll it
 		List<WebElement>values=driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
 		int sum =0;
 		
 		for(int i=0;i<values.size();i++)
 		{
 		System.out.println(Integer.parseInt(values.get(i).getText()));  
-		sum = sum+Integer.parseInt(values.get(i).getText());
+		sum = sum+Integer.parseInt(values.get(i).getText()); //string is converted to integer
 		}
 		System.out.println(sum);
-		int total=Integer.parseInt(driver.findElement(By.cssSelector(".totalAmount")).getText().split(":")[1].trim());
+		int total=Integer.parseInt(driver.findElement(By.cssSelector(".totalAmount")).getText().split(":")[1].trim()); //get text and trim it
 		Assert.assertEquals(sum, total);
 	}
 }
