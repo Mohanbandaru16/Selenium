2df0d74fb601c923b5c93689e298013f01a36629
DELL <DELL@Sivaram>
2021-04-08 09:02:44 +0530

    script to handle a broken link in a web page

---
+++ Selenium/src/com/company/selenium/Brokenlinks.java
@@ -0,0 +1,32 @@
+package com.company.selenium;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Brokenlinks {
+
+	public static void main(String[] args) throws MalformedURLException, IOException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\softwares\\setup of selenium\\SE_Chrome\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		//String url=	driver.findElement(By.cssSelector("a[href*='soapui']")).getAttribute("href");
+		String url=	driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href");
+		
+		HttpURLConnection conn= (HttpURLConnection)new URL(url).openConnection();
+		conn.setRequestMethod("HEAD");
+		conn.connect();
+		int responsecode=conn.getResponseCode();
+		System.out.println(responsecode);
+		
+		
+		
+	}
+
+}
