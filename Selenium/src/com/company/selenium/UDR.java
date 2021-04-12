6af9e51fccfcb26e32533800ef480c26828da4a0
DELL <DELL@Sivaram>
2021-04-12 14:49:57 +0530

    implemented script for taking screenshot before setting value

--- Selenium/src/com/company/selenium/UDR.java
+++ Selenium/src/com/company/selenium/UDR.java
@@ -1,7 +1,13 @@
 package com.company.selenium;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,7 +16,7 @@ import org.openqa.selenium.support.ui.Select;
 
 public class UDR {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\DELL\\Desktop\\softwares\\setup of selenium\\SE_FFOX\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
@@ -23,16 +29,23 @@ public class UDR {
 		driver.findElement(By.xpath("//span[@title='Slot 32, CX3002 [10.100.97.47]']")).click();
 		Thread.sleep(10000);
 		driver.findElement(By.id("chassis_img22")).click();
-		Thread.sleep(15000);
+		Thread.sleep(18000);
 		driver.findElement(By.xpath("//a[normalize-space()='Status']")).click();
 		driver.findElement(By.id("CX_22_20_3-mChnlRxChnl1ResetErrCount")).click();
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//a[normalize-space()='Alarms']")).click();
+	//will take screenshot before changing data	
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src,new File("C:\\Users\\DELL\\Desktop\\New folder\\screenshot.png"));
 	//Alarms 	
 		Actions a = new Actions(driver);
 		WebElement Alarms = driver.findElement(By.xpath("//div[@id='CX_22_20_3-rowIndex7']//div[@class='center']"));
-		a.moveToElement(Alarms).contextClick().sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform();
+	//	a.moveToElement(Alarms).contextClick().sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform();
+		a.moveToElement(Alarms).contextClick().sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform();
 		driver.findElement(By.id("btnApplyAll")).click();
+		Thread.sleep(15000);
+		File src1 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src1,new File("C:\\Users\\DELL\\Desktop\\New folder\\screenshot1.png"));
 	//usersetup tab			
 		driver.findElement(By.xpath("//a[normalize-space()='User Setup']")).click();
 		WebElement usermode = driver.findElement(By.xpath("//select[@id='CX_22_20_3-mChnlRxUserModesSupported1-3']"));
