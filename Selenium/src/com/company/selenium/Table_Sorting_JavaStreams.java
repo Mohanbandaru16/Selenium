e3e890610c712518b12ccdb04de39a43b6cd006d
DELL <DELL@Sivaram>
2021-04-21 09:39:56 +0530

    added code for table pagination

--- Selenium/src/com/company/selenium/Table_Sorting_JavaStreams.java
+++ Selenium/src/com/company/selenium/Table_Sorting_JavaStreams.java
@@ -24,8 +24,30 @@ public class Table_Sorting_JavaStreams {
 		//capture all webelements into new original list
         List<String>originalList=elementsList.stream().map(s->s.getText()).collect(Collectors.toList());
         List<String>sortedList=originalList.stream().sorted().collect(Collectors.toList());
-	
+        //compare originl list with sorted list
         Assert.assertTrue(originalList.equals(sortedList));
+        //scan the name column with get text->Beans->print the price f list
+        List<String>Price;
+        do
+ {
+        	List<WebElement>rows=driver.findElements(By.xpath("//tr/td[1]"));
+        	Price= rows.stream().filter(s->s.getText().contains("Rice")).
+        		map(s->getPriceVeggie(s)).collect(Collectors.toList());
+    //prints
+        Price.forEach(a->System.out.println(a));
+        
+        if(Price.size()<1)
+        {
+        	driver.findElement(By.cssSelector("[aria-label='Next']")).click();
+        }
+ }while(Price.size()<1);      
+        
+	}
+
+	private static String getPriceVeggie(WebElement s) {
+		// TODO Auto-generated method stub
+		String pricevalue= s.findElement(By.xpath("following-sibling::td[1]")).getText();
+		return pricevalue;
 	}
 
 }
