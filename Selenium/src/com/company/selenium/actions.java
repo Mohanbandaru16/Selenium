8887c536d5e451305c99a32f0f281fe1e1eea3f1
Sivaram <ayyagari.sivaram12@gmail.com>
2021-07-27 13:53:49 +0530

    changed Actions program

--- Selenium/src/com/company/selenium/actions.java
+++ Selenium/src/com/company/selenium/actions.java
@@ -18,7 +18,7 @@ public class actions {
 		Actions a= new Actions(driver);
 		WebElement move=driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
 		
-		a.moveToElement(driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+		a.moveToElement(driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"))).click().keyDown(Keys.SHIFT).sendKeys("iphone").doubleClick().build().perform();
 		
 		a.moveToElement(move).contextClick().build().perform();  //moves to specific element 
 				
